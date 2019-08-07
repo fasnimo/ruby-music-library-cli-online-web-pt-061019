8ce0745cdfafdb8e7b64e7438cac083a3ed0a054
Rene Cocom <fasnimo@gmail.com>
2019-08-07 17:45:32 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,10 @@ module Concerns
       end
   end
   def find_or_create_by_name(name)
-      if self.find_by_name(name).nil
+      if self.find_by_name(name).nil?
+          self.create(name)
+      else
+        self.find_by_name(name)
   end
 
 end
