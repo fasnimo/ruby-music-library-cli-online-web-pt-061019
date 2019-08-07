193d471916bcacf2cccfd2e6fd4fd4d9e1602a78
Rene Cocom <fasnimo@gmail.com>
2019-08-07 19:00:53 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -51,7 +51,7 @@ class MusicLibraryController
   end
 
   def list_songs_by_genre
-     puts "Please enter the name of an genre:"
+     puts "Please enter the name of a genre:"
      input = gets.strip
      if genre = Artist.find_by_name(input)
        genre.songs.sort_by {|song| song.name }.each.with_index(1) { |song,idx|
