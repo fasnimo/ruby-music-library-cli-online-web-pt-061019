2097ccc241b06b6684fda5159eebf6be49eca545
Rene Cocom <fasnimo@gmail.com>
2019-08-07 19:26:39 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -28,7 +28,7 @@ class MusicLibraryController
 
     when "list artist"
       list_songs_by_artist
-    when"list genres"
+    when"list genre"
       list_songs_by_genre
     when "play song"
       play_song
