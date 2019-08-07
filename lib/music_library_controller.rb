e91d84190129739c29cf9df45dece9a0ab2b0437
Rene Cocom <fasnimo@gmail.com>
2019-08-07 19:22:42 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -17,6 +17,9 @@ class MusicLibraryController
     puts  "To quit, type 'exit'."
     puts  "What would you like to do?"
     input = gets.strip
+
+    case input
+    when "list songs"
   end
   end
 
